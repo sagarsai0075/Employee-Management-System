6c95bc165eaf551a54437a57e838cb4551136731
Sagar Sai <sagarsai0076@gmail.com>
2026-04-21 15:02:21 +0530

    Created Employee and Department models with admin registration

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    joining_date = models.DateField()
+    address = models.TextField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
