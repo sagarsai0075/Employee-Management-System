1418403b840b6f2d55b09c20aab29678fb57d7a4
Sagar Sai <sagarsai0076@gmail.com>
2026-04-21 15:05:47 +0530

    Created CRUD views for employee management

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Employee
+from .forms import EmployeeForm
 
-# Create your views here.
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'employees/employee_list.html', {'employees': employees})
+
+
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')
+    else:
+        form = EmployeeForm()
+
+    return render(request, 'employees/add_employee.html', {'form': form})
+
+
+def update_employee(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')
+    else:
+        form = EmployeeForm(instance=employee)
+
+    return render(request, 'employees/update_employee.html', {'form': form})
+
+
+def delete_employee(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('employee_list')
+
+    return render(request, 'employees/delete_employee.html', {'employee': employee})
